de0525d7f75c39728275c21179a4df7b5c6b5bf4
rob <rkelly@nmt.edu>
2015-07-03 22:41:21 -0600

    fixed awesify to first display

--- rc.lua
+++ rc.lua
@@ -304,8 +304,10 @@ for s = 1, screen.count() do
    -- Widgets that are aligned to the right
    local right_layout = wibox.layout.fixed.horizontal()
    -- if s == 1 then right_layout:add(wibox.widget.systray()) end
-   right_layout:add(awesify.create_playbox())
-   right_layout:add(awesify.create_musicbox())
+   if s == 1 then
+      right_layout:add(awesify.create_playbox())
+      right_layout:add(awesify.create_musicbox())
+   end
    right_layout:add(mytextclock)
    right_layout:add(mylayoutbox[s])
 
