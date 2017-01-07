96b2498a6e735cf123ddde4927274aa1eb843989
rob <rkelly@nmt.edu>
2017-01-06 19:51:44 -0700

    Awesify tooltips

--- extensions/awesify.lua
+++ extensions/awesify.lua
@@ -15,6 +15,11 @@ awesify.__index = awesify
 -- local play_animation = {' ⣸', '⢀⣰', '⣀⣠', '⣄⣀', '⣆⡀', '⣇ ', '⡏ ', '⠏⠁', '⠋⠉', '⠉⠙', '⠈⠹', ' ⢹'}; local play_box_period = 0.16; local pause_glyph = '⣿⣿';
 local play_animation = {' ⡱', '⢀⡰', '⢄⡠', '⢆⡀', '⢎ ', '⠎⠁', '⠊⠑', '⠈⠱'}; local play_box_period = 0.16667; local pause_glyph = '⢾⡷';
 
+local track_fmt = ' %s <span color="white">%s</span> '
+local tooltip_fmt = '   %s\n' ..
+   '<span color="white">by</span> %s\n' ..
+   '<span color="white">on</span> %s'
+
 function awesify.playpause()
    awful.spawn("dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause")
 end
@@ -27,6 +32,21 @@ function awesify.previous()
    awful.spawn("dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous")
 end
 
+function awesify:set_track_info()
+   if self.track then
+      self.music_box:set_markup(string.format(track_fmt,
+                                              self.track.title,
+                                              self.track.artist))
+      self.tooltip:set_markup(string.format(tooltip_fmt,
+                                            self.track.title,
+                                            self.track.artist,
+                                            self.track.album))
+   else
+      self.music_box:set_text("⣹")
+      self.tooltip:set_markup("... nothing's playing...")
+   end
+end
+
 --- Handler function for PlaybackStatus change signals
 -- Updates the playbox animation to reflect the playback status
 function awesify:handle_playback(status)
@@ -36,6 +56,7 @@ function awesify:handle_playback(status)
    elseif status == "Stopped" then
       self.play_timer:stop()
       self.play_box:set_markup("<span color=\"white\">⣏</span>")
+      self.track = nil
    elseif status == "Playing" then
       self.play_timer:again()
    end
@@ -50,18 +71,16 @@ function awesify:handle_trackchange(metadata)
    end
    if nfields == 0 then
       -- Empty metadata indicates that spotify has been closed
-      self.music_box:set_text("⣹")
+      self.track = nil
    else
+      self.track = {}
       -- Parse and sanitize the data to print
       local title = metadata["xesam:title"] or ""
-      local safe_title = util.sanitize(title)
+      self.track.title = util.sanitize(title)
       local artist_list = metadata["xesam:artist"] or ""
-      local safe_artist = util.sanitize(table.concat(artist_list, ", "))
-
-      self.music_box:set_markup(" " .. safe_title .. " <span color=\"white\">" .. safe_artist .. "</span> ")
-
-      -- Track change implies that a track is playing...
-      self.handle_playback("Playing")
+      self.track.artist = util.sanitize(table.concat(artist_list, ", "))
+      local album = metadata["xesam:album"] or ""
+      self.track.album = util.sanitize(album)
    end
 end
 
@@ -77,17 +96,25 @@ function awesify:on_signal(data, interface, changed, invalidated)
             -- Track change signal
             self:handle_trackchange(changed.Metadata)
          end
+         self:set_track_info()
       end
    end
 end
 
 function awesify.new()
    local self = setmetatable({}, awesify)
+
+   self.track = nil
+
    self.play_index = 1
    self.music_box = wibox.widget.textbox()
-   self.music_box:set_text("⣹")
 
    self.play_box = wibox.widget.textbox()
+
+   local w = wibox.layout.fixed.horizontal(self.play_box, self.music_box)
+
+   self.tooltip = awful.tooltip{objects = {w}, delay_show = 1}
+
    local function animate()
       self.play_index = (self.play_index % #play_animation) + 1
       self.play_box:set_markup("<span color=\"white\">" .. play_animation[self.play_index] .. "</span>")
@@ -96,12 +123,12 @@ function awesify.new()
 
    self.play_timer = timer.start_new(play_box_period, animate)
    self:handle_playback("Stopped")
+   self:set_track_info()
 
    -- Hook into DBus signals
    dbus.add_match("session", "interface='org.freedesktop.DBus.Properties'")
    dbus.connect_signal("org.freedesktop.DBus.Properties", function(...) self:on_signal(...) end)
 
-   local w = wibox.layout.fixed.horizontal(self.play_box, self.music_box)
    return w
 end
 
