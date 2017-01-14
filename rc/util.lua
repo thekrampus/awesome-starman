cdc8ec5ebde3a99d435511aab63c8f70e32cdff1
rob <rkelly@nmt.edu>
2017-01-13 23:02:45 -0700

    Better info in util.log

--- rc/util.lua
+++ rc/util.lua
@@ -6,7 +6,9 @@ local dpi = require("beautiful").xresources.apply_dpi
 local util = {}
 
 local log_date_fmt = "%Y%m%d.%X"
-local log_fmt = "%s <%s> %s: %s"
+local log_stat_fmt = "%8.3fs %8.1fkb"
+local log_fmt = "%s %s: %s%s<%s>"
+local log_width = 100
 
 -- {{{ Helper functions
 function util.conf_debug()
@@ -26,7 +28,9 @@ function util.log(msg, log_fn)
    local caller = debug.getinfo(2)
    local caller_name = caller.name or tostring(caller.func) or "[function]"
    local timestamp = os.date(log_date_fmt)
-   local log_msg = log_fmt:format(timestamp, os.clock(), caller_name, msg)
+   local stats = log_stat_fmt:format(os.clock(), collectgarbage("count"))
+   local padding = string.rep(" ", log_width - (#caller_name + #msg + #stats + 5))
+   local log_msg = log_fmt:format(timestamp, caller_name, msg, padding, stats)
    log_fn(log_msg)
 end
 
