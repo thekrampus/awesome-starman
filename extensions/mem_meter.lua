3f7d2773dbd2a2a411ed4ac7f5b14a65add294b1
Rob Kelly <rkelly@mro.nmt.edu>
2018-05-09 19:51:42 -0600

    mem_meter uses asynchronous polling

--- extensions/mem_meter.lua
+++ extensions/mem_meter.lua
@@ -1,6 +1,6 @@
 -- A widget to poll and display useful memory usage info
 local setmetatable = setmetatable
-local util = require("rc.util")
+local spawn = require("awful.spawn")
 local textbox = require("wibox.widget.textbox")
 local timer = require("gears.timer")
 local mem_meter = { mt = {} }
@@ -61,11 +61,18 @@ function mem_meter.new(total_width, timeout)
    local w = textbox()
 
    local function poll()
-      local memstr = util.pread("free --kilo 2>&1")
-      local total, used, swap = mem_meter.parseUsage(memstr)
-      local markup = mem_meter.make_readout(total, used, swap, total_width)
-
-      w:set_markup(markup)
+      spawn.easy_async("free --kilo 2>&1",
+                       function(out, err, _, code)
+                          if code ~= 0 then
+                             print("\nNonzero exit code from cpu_meter usage call: " .. code)
+                             print(err)
+                          else
+                             local total, used, swap = mem_meter.parseUsage(out)
+                             local markup = mem_meter.make_readout(total, used, swap, total_width)
+                             w:set_markup(markup)
+                          end
+                       end
+      )
       return true
    end
 
