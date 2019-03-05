86fbd2f82cde08610dc20dbc9e27a08ecf87351e
Rob Kelly <rpkelly@sandia.gov>
2019-03-05 09:35:14 -0700

    Freedesktop menu

--- rc/menu.lua
+++ rc/menu.lua
@@ -1,5 +1,10 @@
 -- Menu configuration (called from rc.lua)
 local menu = require("awful.menu")
 local var  = require("rc.variables")
+local freedesktop = require("freedesktop")
 
-return menu{ items = var.mainmenu }
+local _menu = {}
+_menu.main = menu{ items = var.mainmenu }
+_menu.freedesktop = freedesktop.menu.build()
+_menu.client = function() awful.menu.client_list({ theme = { width = 250 } }) end
+return _menu
